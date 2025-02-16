e277309f213d72f8f90ac5f0b99960e1ab6eb8f0
Tai Kedzierski <dch.tai@gmail.com>
2025-02-16 23:21:11 +0000

    Working basic with global definitions

--- goargs/goargs.go
+++ goargs/goargs.go
@@ -3,13 +3,33 @@ package goargs
 import (
     "fmt"
     "os"
+    "strconv"
 )
 
 
-type VarDef struct {
+type VarDef interface {
+    GetName() string
+    Assign(string)
+}
+
+type StringDef struct {
     name string
     value *string
-    defval string
+}
+func (self StringDef) GetName() string { return self.name }
+func (self StringDef) Assign(value string) { *self.value = value }
+
+type IntDef struct {
+    name string
+    value *int
+}
+func (self IntDef) GetName() string { return self.name }
+func (self IntDef) Assign(value string) {
+    if value, err := strconv.Atoi(value); err != nil {
+        panic(fmt.Sprintf("Could not parse %s\n", value) )
+    } else {
+        *self.value = value
+    }
 }
 
 
@@ -17,14 +37,20 @@ var definitions = []VarDef{}
 
 
 func StringArg(value *string, name string, defval string) {
-    definitions = append(definitions, VarDef{name, value, defval} )
+    s := StringDef{name, value}
+    *s.value = defval
+    definitions = append(definitions, s)
+}
+
+func IntArg(value *int, name string, defval int) {
+    s := IntDef{name, value}
+    *s.value = defval
+    definitions = append(definitions, s)
 }
 
 func Parse() {
-    if len(os.Args) == 1 {
-        *definitions[0].value = definitions[0].defval
-    } else {
-        *definitions[0].value = os.Args[1]
+    for i, str := range os.Args[1:] {
+        definitions[i].Assign(str)
     }
 }
 
@@ -32,8 +58,10 @@ func Parse() {
 
 func SpotCheck() {
     var person string
+    var age int
     StringArg(&person, "name", "nobody")
+    IntArg(&age, "age", -1)
     Parse()
-    fmt.Println(person)
+    fmt.Printf("%s -> %d\n", person, age)
     fmt.Printf("%v\n", definitions)
 }
