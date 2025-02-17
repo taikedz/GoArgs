742668126012a01a56ee0ee08452d8224f22611d
Tai Kedzierski <dch.tai@gmail.com>
2025-02-17 00:26:47 +0000

    Working basic flag parser!!

--- goargs/goargs.go
+++ goargs/goargs.go
@@ -14,7 +14,52 @@ type VarDef interface {
 
 type Parser struct {
     definitions []VarDef
+    positionals []string
+    passdown_args []string
 }
+
+func (p *Parser) fromName(longname string) *VarDef {
+    for _, vdef := range p.definitions {
+        if vdef.GetName() == longname { return &vdef }
+    }
+    return nil
+}
+
+func (p *Parser) Parse(args []string) error {
+    for i := 0; i<len(args); i++ {
+        token := args[i]
+        var def VarDef = nil
+
+        if token[:2] == "--" {
+            longname := token[2:]
+            if longname == "" {
+                // We found the first delimiter for passdown arguments
+                // Retain them as such, and stop parsing
+                p.passdown_args = args[i+1:]
+                return nil
+            }
+            def = *p.fromName(longname)
+
+        }
+
+        // TODO - support short names?
+
+        if def != nil {
+            i++
+            // TODO - this won't apply to bool flag - check type
+            // https://stackoverflow.com/a/7006853/2703818
+            if i >= len(args) { return fmt.Errorf("Expected value after %s", token) }
+
+            def.Assign(args[i])
+        } else {
+            p.positionals = append(p.positionals, token)
+        }
+    }
+
+    return nil
+}
+
+// Supported types
 
 func (p *Parser) StringArg(value *string, name string, defval string) {
     s := StringDef{name, value}
@@ -28,14 +73,6 @@ func (p *Parser) IntArg(value *int, name string, defval int) {
     p.definitions = append(p.definitions, s)
 }
 
-func (p *Parser) Parse(args []string) {
-    for i, str := range args {
-        if i >= len(p.definitions) { return }
-
-        p.definitions[i].Assign(str)
-    }
-}
-
 // ---------
 
 func SpotCheck() {
@@ -47,7 +84,8 @@ func SpotCheck() {
     par.IntArg(&age, "age", -1)
     par.Parse(os.Args[1:])
 
-    fmt.Printf("Definitions: %v\n", par.definitions)
+    fmt.Printf("Parser: %v\n", par)
     fmt.Printf("%s -> %d\n", person, age)
     // */
 }
+
