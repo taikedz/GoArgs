d5d312f716254f44bbd55341ea5a0f66c10b7b44
Tai Kedzierski <dch.tai@gmail.com>
2026-08-26 11:12:07 +0100

    WIP

--- goargs/goargs.go
+++ goargs/goargs.go
@@ -9,11 +9,22 @@ Minimal requirements
 - positional and flag options can appear in any order
 - short flags available for options
 - support for specifying mutually exclusive options
+- help printing
 - support for basic types int, uint, string, float, bool
 - support for adding a parser of custom type
 */
 
 import "github.com/taikedz/goargs/goargs"
+
+type Argument interface {
+	Required() bool
+	Short(rune)
+}
+
+type ModeArgument interface {
+	Argument
+	ModeShorts([]rune)
+}
 
 func Parse(tokens []string) {
 
@@ -26,10 +37,12 @@ func Parse(tokens []string) {
 	var uid int
 	create_home := false
 	var mode string
+	purge := false
 
 	// Subcommand
-	var subc string
-	subc_action := main_parser.Subcommand([]string{"create", "delete"}, "Create or delete a user")
+	subc_action := main_parser.Subcommand("action", []string{"create", "delete"}, "Create or delete a user")
+
+	// ----- User reation subcommand
 	create_parser := subc_action.ParserFor("create")
 
 	// Positional arguments have names not starting with "-"
@@ -42,6 +55,17 @@ func Parse(tokens []string) {
 	mode_param := create_parser.Mode(&mode, []string{"user", "admin", "service"}, "What type of user to create")
 	mode_param.Short("-m")
 	mode_param.ModeShorts([]rune{'U', 'A', 'S'}) // 1:1 corresponding to choices
+
+	// ------- User deletion subcommand
+	delete_parser := subc_action.ParserFor("delete")
+
+	// Note - this references a variable controlled elsewhere too ; but the mode is mutually exclusive
+	// leave it to the consuming developer to decide
+	delete_parser.String(&username, "name", "Name of user account")
+
+	delete_parser.Bool(&purge, "--purge", "Whether to remove home folder")
+
+	// ========
 
 	main_parser.Parse(
 		tokens,
